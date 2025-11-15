c24f18e9b30744be21877d37ae65be542599fa52
Larry Botha <lawrenceguy@gmail.com>
2025-11-15 16:09:42 +0200

    refactor(03-13): clarify array decaying to pointer when passed to function

--- 03/13-arrays-decay-to-pointers/main.c
+++ 03/13-arrays-decay-to-pointers/main.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
 
 // arrays decay to pointers when passed as values to functions
-void some_func(const int xs_as_arg[]) {
-  printf("xs as arg to func:\n");
-  printf("&xs_as_arg: %p\n",
-         &xs_as_arg); // address of the _pointer_ to xs_as_arg
-  printf("xs_as_arg:  %p\n", xs_as_arg); // address of xs_as_arg
+/*void some_func(const int xs[]) {*/
+void some_func(const int *xs // this more accurately describes the array
+                             // decaying to a pointer
+) {
+  printf("xs as arg to func:\n\n");
 
-  // size of the _pointer_ to xs_as_arg
-  printf("sizeof(xs_as_arg) in some_func: %zu\n", sizeof(xs_as_arg));
-  printf("                                ^\n");
-  printf("---------------------------------\n");
-  printf("|\n");
-  printf("this is the size of the _pointer_, not the array\n");
+  printf("*xs:        %d\n\n", *xs);
+
+  printf("xs:         %p\n", xs);
+  printf("&xs:        %p (address of pointer!)\n", &xs);
+  printf("&xs[0]:     %p\n", &xs[0]);
+
+  // size of the _pointer_ to xs
+  printf("sizeof(xs): %zu (size of pointer!)\n", sizeof(xs));
 }
 
 int main() {
   int xs[] = {43, 67, 89, 92, 71, 43, 85};
   int len = sizeof(xs) / sizeof(xs[0]);
 
-  // xs decays to a pointer, as if we used xs&
-  printf("&xs:           %p\n", &xs);
-  printf("xs:            %p\n", xs);
-
-  printf("sizeof(xs[0]): %zu\n", sizeof(xs[0]));
   printf("len of xs:     %d\n", len);
+  printf("sizeof(xs[0]): %zu\n", sizeof(xs[0]));
+  printf("*xs:           %d\n", *xs);
+  printf("xs[0]:         %d\n", xs[0]);
+
+  printf("\nxs where defined:\n\n");
+
+  // xs decays to a pointer, as if we used xs&
+  printf("xs:            %p\n", xs);
+  printf("&xs:           %p\n", &xs);
+  printf("&xs[0]:        %p\n", &xs[0]);
+
   // xs does not decay when passed to sizeof, otherwise we'd get the
   // size of the pointer
-  printf("sizeof(xs):    %zu\n", sizeof(xs));
+  printf("sizeof(xs):    %zu (i.e. len(xs) * size(xs[0]))\n", sizeof(xs));
 
   printf("\n");
 
