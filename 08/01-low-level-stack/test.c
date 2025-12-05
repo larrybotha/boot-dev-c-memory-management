cf980464ae252b2349ee803dc68f22507b5a7a80
Larry Botha <lawrenceguy@gmail.com>
2025-12-05 22:05:15 +0200

    test(08/01): add test for larger stack

--- 08/01-low-level-stack/test.c
+++ 08/01-low-level-stack/test.c
@@ -4,13 +4,23 @@
 
 MunitResult test_create_stack_small() {
 
-	stack_t *s = alloc_stack(3);
+  stack_t *s = alloc_stack(3);
 
-	munit_assert_int(s->capacity, ==, 3);
-	munit_assert_int(s->count, ==, 0);
-	munit_assert_ptr_not_null(s->data);
+  munit_assert_int(s->capacity, ==, 3);
+  munit_assert_int(s->count, ==, 0);
+  munit_assert_ptr_not_null(s->data);
 
-	return MUNIT_OK;
+  return MUNIT_OK;
+}
+
+MunitResult test_create_stack_large() {
+  stack_t *s = alloc_stack(100);
+
+  munit_assert_int(s->capacity, ==, 100);
+  munit_assert_int(s->count, ==, 0);
+  munit_assert_ptr_not_null(s->data);
+
+  return MUNIT_OK;
 }
 
 int main(int argc, char **argv) {
@@ -18,6 +28,14 @@ int main(int argc, char **argv) {
       {
           "/test_create_stack_small",
           test_create_stack_small,
+          NULL,
+          NULL,
+          MUNIT_TEST_OPTION_NONE,
+          NULL,
+      },
+      {
+          "/test_create_stack_large",
+          test_create_stack_large,
           NULL,
           NULL,
           MUNIT_TEST_OPTION_NONE,
